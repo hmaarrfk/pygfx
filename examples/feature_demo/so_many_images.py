b7d73ff0a131c54b504efe5c1b5ca64c174c5b69
Mark Harfouche <mark.harfouche@gmail.com>
2024-07-07 14:46:07 -0400

    Better example

--- examples/feature_demo/so_many_images.py
+++ examples/feature_demo/so_many_images.py
@@ -1,8 +1,21 @@
+import os
 import numpy as np
 import imageio.v3 as iio
 from wgpu.gui.auto import WgpuCanvas, run
 import pygfx as gfx
 
+if "PYTEST_CURRENT_TEST" not in os.environ:
+    import argparse
+    parser = argparse.ArgumentParser(description="Frustum Culling Demo")
+    # make it a flag with the ability to use no as a prefix
+    parser.add_argument(
+        "--frustum-culling", action="store_true", help="Enable frustum culling"
+    )
+
+    args = parser.parse_args()
+    frustum_culling = args.frustum_culling
+else:
+    frustum_culling = False
 
 canvas = WgpuCanvas()
 renderer = gfx.renderers.WgpuRenderer(canvas)
@@ -23,13 +36,14 @@ for i in np.ndindex(image_matrix_shape):
             clim=(0 + i[0] * 5, 255 - i[1] * 5),
             opacity=0.5,
         ),
-        frustum_culling=True,
+        frustum_culling=frustum_culling,
     )
-    image.local.position = (i[1] * im_shape[1] * 1.1, i[0] * im_shape[0] * 1.1, 0)
+    image.local.position = (
+        i[1] * im_shape[1] * 1.1 - 256,
+        i[0] * im_shape[0] * 1.1 - 256,
+        0,
+    )
     scene.add(image)
-
-
-
 
 camera = gfx.OrthographicCamera(512, 512)
 camera.local.scale_y = -1
