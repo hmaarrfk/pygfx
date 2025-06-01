677c488f7569f8659110c89935b24d483637618d
Mark Harfouche <mark.harfouche@gmail.com>
2025-06-01 15:26:04 -0400

    Try to add transparency to the bbox in the selectors

--- examples/validation/validate_line_dash.py
+++ examples/validation/validate_line_dash.py
@@ -18,68 +18,43 @@ import pygfx as gfx
 canvas = WgpuCanvas(size=(1000, 1000))
 renderer = gfx.WgpuRenderer(canvas)
 
-x = np.linspace(0, 4 * np.pi, 1000)
-y = np.sin(x)
 
-
-positions = np.array([x * 100, y * 100, np.zeros_like(x)], np.float32).T.copy()
-
-geometry = gfx.Geometry(positions=positions)
-
-line1 = gfx.Line(
-    geometry,
-    gfx.LineMaterial(
-        thickness=12,
-        dash_pattern=[0, 2, 2, 2],
-        color=(0.0, 1.0, 1.0, 0.4),
-        dash_offset=0,
-    ),
-)
-
-line2 = gfx.Line(
-    geometry,
-    gfx.LineMaterial(
-        thickness=12,
-        dash_pattern=[0, 2, 2, 2],
-        color=(0.0, 1.0, 1.0, 0.4),
-        dash_offset=2,
-    ),
-)
-line3 = gfx.Line(
-    geometry,
-    gfx.LineMaterial(
-        thickness=24,
-        dash_pattern=[0, 2, 2, 2],
-        color=(0.0, 1.0, 1.0, 0.4),
-        dash_offset=3,
-    ),
-)
-
-line4 = gfx.Line(
-    geometry,
-    gfx.LineMaterial(
-        thickness=24,
-        dash_pattern=[0, 2, 2, 2],
-        color=(0.0, 1.0, 1.0, 0.4),
-        dash_offset=6,
-    ),
-)
-
-for line in (line1, line2, line3, line4):
-    line.material.thickness_space = "screen"
-
-line1.local.position = 0, 0, 0
-line2.local.position = 0, -500, 0
-line3.local.position = 0, -1000, 0
-line4.local.position = 0, -1500, 0
-
+N = 48_000
+# N_cameras = (6, 4)
+N_cameras = (12, 8)
+lines = np.zeros(N_cameras, dtype=object)
 scene = gfx.Scene()
-scene.add(line1, line2, line3, line4)
+material = gfx.LineMaterial(
+    thickness=12,
+    # dash_pattern=[0, 2, 2, 2],
+    color=(0.0, 1.0, 1.0, 1.),
+    # dash_offset=0,
+)
+
+for i in np.ndindex(N_cameras):
+    x = np.sin((np.random.rand(N) * 2 * np.pi).cumsum() / 1000)
+    y = np.cos((np.random.rand(N) * 2 * np.pi).cumsum() / 1000)
+
+    positions = np.ascontiguousarray(np.array([
+        x * 100,
+        y * 100,
+        np.zeros_like(x)
+    ], np.float32).T)
+
+    line = gfx.Line(
+        gfx.Geometry(positions=positions),
+        material=material,
+    )
+    line.material.thickness_space = "screen"
+    line.local.position = 220 * i[0], 220 * i[1], 0
+    scene.add(line)
+    lines[i] = line
 
 camera = gfx.OrthographicCamera()
 camera.show_object(scene)
 
-controller = gfx.OrbitController(camera, register_events=renderer)
+# controller = gfx.OrbitController(camera, register_events=renderer)
+controller = gfx.PanZoomController(camera, register_events=renderer)
 
 canvas.request_draw(lambda: renderer.render(scene, camera))
 
